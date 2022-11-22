d15347d00aa44c408aea1bbe31742102819cb26f
calvintzeng96 <calvintzeng96@hotmail.com>
2022-11-22 03:19:40 -0500

    user columns done

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from sqlalchemy.orm import validates
 
 
 class User(db.Model, UserMixin):
@@ -31,9 +32,16 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+
+
+
+
+
+
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "profilePicture": self.profile_picture,
         }
