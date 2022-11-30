240af211b3407202b1e6c7eea9fa37aef74b5d6c
calvintzeng96 <calvintzeng96@hotmail.com>
2022-11-30 18:28:48 -0500

    post edit done

--- react-app/src/components/EditPost/index.js
+++ react-app/src/components/EditPost/index.js
@@ -18,6 +18,8 @@ const EditPost = () => {
     const { setModalType } = useContext(ModalContext)
     const post = useSelector(state => state.post.singlePost)
     const [postId, setPostId] = useState("")
+    const [imageLoading, setImageLoading] = useState(false)
+    const [imageRemoval, setImageRemoval] = useState("Remove Existing Image")
 
     useEffect(() => {
         setPostId(post.id)
@@ -27,19 +29,75 @@ const EditPost = () => {
     useEffect(() => {
         setContent(post.content)
         setImage(post.image)
-    }, [post])
+        console.log(post.content)
+        console.log(post.image)
+    }, [])
 
-    const submit = (e) => {
+    const submit = async (e) => {
         e.preventDefault();
-        setErrors([]);
-
-        const data = { image, content };
-
-        dispatch(updatePost(postId, data))
-            .then(() => {
-                setModalType(false)
+        // setErrors([]);
+        const formData = new FormData();
+        if (imageRemoval == "Existing image removed :)") {
+            console.log("pppppppppppppppppp")
+            setImage("remove existing image")
+            await fetch('/api/posts/checkImage', {
+                method: "POST",
+                body: image
             })
+                .then(() => {
+                    console.log("ooooooooooooooooooo")
+                    let data = {
+                        "image": "remove existing image",
+                        content
+                    }
+                    dispatch(updatePost(postId, data))
+                        .then(() => {
+                            setModalType(false)
+                        })
+                })
+
+        }
+        formData.append("image", image);
+        setImageLoading(true);
+        await fetch('/api/posts/checkImage', {
+            method: "POST",
+            body: formData
+        })
+            .then(async (res) => {
+                let imgurl = await res.text()
+                if (imgurl.includes("not permitted")) {
+                    console.log("*******************1")
+                    setErrors(["Only png/jpg/jpeg/gif allowed"])
+                    return
+                }
+                let data = { image: imgurl, content };
+                // console.log(imgurl)
+                if (imgurl.includes("!DOCTYPE")) {
+                    console.log("*******************2")
+                    console.log("HERE")
+                    data = { content }
+                }
+                console.log(content)
+                dispatch(updatePost(postId, data))
+                    .then(() => {
+                        setModalType(false)
+                    })
+            })
+
+        // const data = { image, content };
+
     };
+
+    const updateImage = (e) => {
+        const file = e.target.files[0];
+        console.log("99999999999999999999", e.target.files)
+        setImage(file);
+    }
+
+    const removeImage = () => {
+        // setImage("remove existing image")
+        setImageRemoval("Existing image removed :)")
+    }
 
     return (
         <form className="form-container modal-content" onSubmit={submit}>
@@ -56,11 +114,11 @@ const EditPost = () => {
                 placeholder={`What's on your mind, ${currentUser.firstName}?`}
             />
             <input
-                type="text"
-                value={image}
-                onChange={(e) => setImage(e.target.value)}
-                placeholder="Image url"
+                type="file"
+                accept="image/*"
+                onChange={updateImage}
             />
+            <div className="cursor" onClick={() => removeImage()}>{imageRemoval}</div>
             <button type="submit">Post</button>
         </form>
     )
